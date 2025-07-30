373a934ee92b9474d072ae1590f78ff4d682e1d2
John Paul Dala <jaypeedala31@gmail.com>
2025-07-31 01:27:53 +0800

    implement partial image module

---
+++ src/hooks/api/useImagesApi.js
@@ -0,0 +1,18 @@
+// src/hooks/api/useImagesApi.js
+import { useApi } from "@/hooks/useApi";
+import { API_PREFIX } from "@/constants/api";
+
+export const useImagesApi = () => {
+    const api = useApi();
+    return {
+        list: (params) =>
+            api.get(`${API_PREFIX}/admin/images`, { params, requiresAuth: true }),
+        create: (formData) =>
+            api.post(`${API_PREFIX}/admin/images`, formData, {
+                requiresAuth: true,
+                headers: { "Content-Type": "multipart/form-data" },
+            }),
+        remove: (id) =>
+            api.delete(`${API_PREFIX}/admin/images/${id}`, { requiresAuth: true }),
+    };
+};
