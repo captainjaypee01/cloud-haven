de1c204fe9231a14ac33f08045ede56692ace6ad
John Paul Dala <jaypeedala31@gmail.com>
2025-08-27 00:20:10 +0800

    update home page and fix room details

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,62 +1,63 @@
 import React from 'react'
-import { assets } from '../assets/assets'
+import { assets } from '@/assets/assets'
+import { Link } from "react-router-dom";
 
 const Footer = () => {
     return (
         <div className='bg-[#F6F9FC] text-gray-500/80 pt-8 px-6 md:px-16 lg:px-24 xl:px-32'>
             <div className='flex flex-wrap justify-between gap-12 md:gap-6'>
                 <div className='max-w-80'>
-                    <img src={assets.testLogo} alt="logo" className='mb-4 h-8 md:h-9' />
+                    <img src={assets.logo} alt="logo" className='mb-4 h-8 md:h-9' />
                     <p className='text-sm'>
                         Discover the world's most extraordinary place to stay, from botique hotels to luxury villas and private islands.
                     </p>
                     <div className='flex items-center gap-3 mt-4'>
                         {/* Instagram */}
-                        <img src={assets.instagramIcon} alt='instagram-icon' className='w-6' />
+                        <a href="https://www.instagram.com/netaniadelaiya" target="_blank" rel="noopener noreferrer" className="text-sky-600 hover:underline">
+                            <img src={assets.instagramIcon} alt='instagram-icon' className='w-6' />
+                        </a>
                         {/* Facebook */}
-                        <img src={assets.facebookIcon} alt='facebook-icon' className='w-6' />
-                        {/* Twitter */}
-                        <img src={assets.twitterIcon} alt='twitter-icon' className='w-6' />
-                        {/* LinkedIn */}
-                        <img src={assets.linkendinIcon} alt='linkedin-icon' className='w-6' />
+                        <a href="https://www.facebook.com/profile.php?id=100064182843841" target="_blank" rel="noopener noreferrer" className="text-sky-600 hover:underline">
+                            <img src={assets.facebookIcon} alt='facebook-icon' className='w-6' />
+                        </a>
                     </div>
                 </div>
 
                 <div>
                     <p className='font-playfair text-lg text-gray-800'>Netania De Laiya</p>
                     <ul className='mt-3 flex flex-col gap-2 text-sm'>
-                        <li><a href="#">About</a></li>
-                        <li><a href="#">Careers</a></li>
-                        <li><a href="#">Press</a></li>
-                        <li><a href="#">Blog</a></li>
-                        <li><a href="#">Partners</a></li>
+                        <li>
+                            <Link to={`/about-us`}>About</Link>
+                        </li>
+                        <li>
+                            <Link to={`/rooms`}>Accommodations</Link>
+                        </li>
+                        <li>
+                            <Link to={`/policy`}>Policy</Link>
+                        </li>
                     </ul>
                 </div>
 
                 <div>
                     <p className='font-playfair text-lg text-gray-800'>SUPPORT</p>
                     <ul className='mt-3 flex flex-col gap-2 text-sm'>
-                        <li><a href="#">Help Center</a></li>
-                        <li><a href="#">Safety Information</a></li>
-                        <li><a href="#">Cancellation Options</a></li>
-                        <li><a href="#">Contact Us</a></li>
-                        <li><a href="#">Accessibility</a></li>
+                        <li>
+                            <Link to={`/contact-us`}>Contact Us</Link>
+                        </li>
                     </ul>
                 </div>
 
                 <div className='max-w-80'>
-                    <p className='font-playfair text-lg text-gray-800'>STAY UPDATED</p>
+                    <p className='font-playfair text-lg text-gray-800'>GET IN TOUCH</p>
                     <p className='mt-3 text-sm'>
-                        Subscribe to our newsletter for inspiration and special offers.
+                        Laiya-Aplaya,San Juan,Batangas, Batangas City, Philippines
                     </p>
-                    <div className='flex items-center mt-4'>
-                        <input type="text" className='bg-white rounded-l border border-gray-300 h-9 px-3 outline-none' placeholder='Your email' />
-                        <button className='flex items-center justify-center bg-black h-9 w-9 aspect-square rounded-r'>
-                            {/* Arrow icon */}
-
-                            <img src={assets.arrowIcon} alt='arrow-icon' className='w-3.5 invert' />
-                        </button>
-                    </div>
+                    <p className='mt-3 text-sm'>
+                        Phone: +63 949 798 9831
+                    </p>
+                    <p className='mt-3 text-sm'>
+                        Email: info@netaniadelaiya.com
+                    </p>
                 </div>
             </div>
             <hr className='border-gray-300 mt-8' />
